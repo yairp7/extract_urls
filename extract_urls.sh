42271602470b3a1b01948a942662f4df1cd0a677
yair.pecherer <yair.pecherer@similarweb.com>
2019-02-20 19:24:22 +0200

    fix

--- extract_urls.sh
+++ extract_urls.sh
@@ -18,6 +18,29 @@ FILENAME=$1
 OUTPUT='output.txt'
 REPORT=$2
 
+function process_binary {
+	urls=$(rabin2 -z $1 | egrep -w 'https?://[^ ]+')
+	if [ ! -z "${urls}" ]; then
+		{ printf "@${1}[binary]: \n"; rabin2 -z $1 | egrep -w 'https?://[^ ]+'; } >> $2
+	fi
+}
+
+function process_plaintext {
+	urls=$(strings $1 | egrep -o 'https?://[^ ]+')
+	if [ ! -z "${urls}" ]; then
+		{ printf "@${1}[plaintext]: \n"; strings $1 | egrep -o 'https?://[^ ]+'; } >> $2
+	fi
+}
+
+function is_binary() {
+	filetype=$(file -b --mime-type $1 | sed 's|/.*||')
+	if [[ $filetype == *"application"* ]]; then
+		echo "bin"
+	else
+		echo "txt"
+	fi
+}
+
 # Check if APK file
 if [[ $FILENAME == *"apk"* ]]; then
 	echo "[+] APK file found, extracting..."
@@ -28,13 +51,23 @@ if [[ $FILENAME == *"apk"* ]]; then
 	unzip -o -q "${FILENAME}" -d "./tmp/unzip_result"
 	apktool --quiet -f d "${FILENAME}" -o "./tmp/apktool_result"
 
+	# TMP
+	rm -rf ./tmp/unzip_result/res
+	rm -rf ./tmp/apktool_result/res
+	# END TMP
+
 	echo "### Urls Found:" > $OUTPUT
 
 	fileCount=0
 	IFS=$'\n' files=( $(find "./tmp" -type f) )
-	for d in "${files[@]}"; do
+	for f in "${files[@]}"; do
 		fileCount=$(( fileCount+1 ))
-    	strings $d | egrep -o 'https?://[^ ]+' >> $OUTPUT
+		result=$(is_binary $f)
+		if [[ $result == *"bin"* ]]; then
+			process_binary $f $OUTPUT
+		else
+			process_plaintext $f $OUTPUT
+		fi
     	echo -ne "[+] Files Checked: [${fileCount} / ${#files[@]}]\r";
 	done
 	echo -e "\r"
@@ -43,8 +76,9 @@ if [[ $FILENAME == *"apk"* ]]; then
 else
 	echo "[+] Checking file..."
 	echo "### Urls Found:" > $OUTPUT
-	strings $FILENAME | egrep -o 'https?://[^ ]+' >> $OUTPUT
+	process_plaintext $f $OUTPUT
 fi
-echo "[+] Cleaning duplicated urls..."
-python remove_duplicates.py $OUTPUT $REPORT
+# echo "[+] Cleaning duplicated urls..."
+# python remove_duplicates.py $OUTPUT $REPORT
+cp $OUTPUT $REPORT
 rm $OUTPUT
