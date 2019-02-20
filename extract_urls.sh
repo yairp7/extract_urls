e33e5ec018075579408c7d013047e115c83ae185
yair.pecherer <yair.pecherer@similarweb.com>
2019-02-20 21:17:51 +0200

    20/02/2019

--- extract_urls.sh
+++ extract_urls.sh
@@ -7,16 +7,12 @@ then
 	exit 1
 fi
 
-if [ -z "$2" ] 
-then
-	echo "[-] Must provide an output file"
-	echo "[-] Syntax: extract_urls.sh <input> <output>"
-	exit 1
+if [ ! -z "$2" ]; then
+	REPORT=$2
 fi
 
 FILENAME=$1
 OUTPUT='output.txt'
-REPORT=$2
 
 function process_binary {
 	urls=$(rabin2 -z $1 | egrep -w 'https?://[^ ]+')
@@ -41,6 +37,17 @@ function is_binary() {
 	fi
 }
 
+function process {
+	result=$(is_binary $1)
+		if [[ $result == *"bin"* ]]; then
+			process_binary $1 $2
+		else
+			process_plaintext $1 $2
+		fi
+}
+
+echo "### Urls Found:" > $OUTPUT
+
 # Check if APK file
 if [[ $FILENAME == *"apk"* ]]; then
 	echo "[+] APK file found, extracting..."
@@ -51,23 +58,11 @@ if [[ $FILENAME == *"apk"* ]]; then
 	unzip -o -q "${FILENAME}" -d "./tmp/unzip_result"
 	apktool --quiet -f d "${FILENAME}" -o "./tmp/apktool_result"
 
-	# TMP
-	rm -rf ./tmp/unzip_result/res
-	rm -rf ./tmp/apktool_result/res
-	# END TMP
-
-	echo "### Urls Found:" > $OUTPUT
-
 	fileCount=0
 	IFS=$'\n' files=( $(find "./tmp" -type f) )
 	for f in "${files[@]}"; do
 		fileCount=$(( fileCount+1 ))
-		result=$(is_binary $f)
-		if [[ $result == *"bin"* ]]; then
-			process_binary $f $OUTPUT
-		else
-			process_plaintext $f $OUTPUT
-		fi
+		process $f $OUTPUT
     	echo -ne "[+] Files Checked: [${fileCount} / ${#files[@]}]\r";
 	done
 	echo -e "\r"
@@ -75,10 +70,14 @@ if [[ $FILENAME == *"apk"* ]]; then
 	rm -rd "./tmp"
 else
 	echo "[+] Checking file..."
-	echo "### Urls Found:" > $OUTPUT
-	process_plaintext $f $OUTPUT
+	
+	process $FILENAME $OUTPUT
 fi
 # echo "[+] Cleaning duplicated urls..."
 # python remove_duplicates.py $OUTPUT $REPORT
-cp $OUTPUT $REPORT
+if [ -z "$REPORT" ]; then
+	cat $OUTPUT
+else
+	cp $OUTPUT $REPORT
+fi
 rm $OUTPUT
